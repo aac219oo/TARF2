9c90b354a0ead71697eb8ba815796000c59eeb82
JamesHsu <aac921oo@gmail.com>
2022-10-04 15:47:28 +0800

    try importing header.vue

---
+++ src/components/header.vue
@@ -0,0 +1,12 @@
+<template>
+    <el-container>
+      <el-header>
+        <a href="./index.html">
+          <h1>TARF2技術文件線上簽核系統</h1>
+        </a>
+        <h3>系統入口</h3>
+        <p> </p>
+      </el-header>
+    </el-container>
+</template>
+
