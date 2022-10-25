dd6a50401f9b26033bc3f1eb19c8502116d7dc92
JAMES\aac219oo <aac921oo@gmail.com>
2022-10-26 02:06:07 +0800

    addItem fixed in individual_words.vue

--- src/pages/document_search/contact_units.vue
+++ src/pages/document_search/contact_units.vue
@@ -1,46 +1,45 @@
 <script lang="ts" setup>
-import { ref , reactive , onMounted , watch } from 'vue'
-import { Search } from '@element-plus/icons-vue'
-import axios from 'axios';
+import { ref, reactive, onMounted, watch, computed } from "vue";
+import { Search } from "@element-plus/icons-vue";
+import axios from "axios";
 
 //navbar
-const activeIndex = ref('1')
+const activeIndex = ref("1");
 const handleSelect = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
+  console.log(key, keyPath);
+};
 
 // filter
-const input1 = ref('')
+const search = ref("");
+
+const tabledata = reactive({
+  newsdata: null,
+});
 
 // axios
-const url = 'http://tarf.grp.com.tw/api/Test/GetDeptChargQuery';
+const url = "http://tarf.grp.com.tw/api/Test/GetDeptChargQuery";
 
-const data = reactive({
-    newsdata:'',
-})
-
-const tableInfo = []
-  
 onMounted(() => {
-  axios.get(url)
-    .then(res => {
-      console.log(res.data);
-      data.newsdata = res.data
+  axios
+    .get(url)
+    .then((res) => {
+      // console.log(res.data);
+      // tabledata.newsdata = res.data;
+      console.log(res);
+      tabledata.newsdata = res.data.newsdata;
     })
     .catch(function (error) {
       // handle error
-      console.log(error)
+      console.log(error);
     })
     .then(function () {
       // always executed
-    })
+    });
 });
 
-
-
+// console.log(tabledata);
 
 // table
-// const input1 = ref('')
 // const tableInfo = [
 //         {
 //     depT_NAME: '綜規處',
@@ -88,52 +87,74 @@ onMounted(() => {
 //     ofF_TEL: '02-25215550-13',
 //         },
 // ]
-
 </script>
 <template>
-    <el-container>
-      <el-header>
-        <a href="../../index.html">
-          <h1>TARF2技術文件線上簽核系統</h1>
-        </a>
-        <h3>窗口資訊查詢</h3>
-        <p>蘇家淇(11548)</p>
-      </el-header>
-      <el-main>
-        <el-menu
-    :default-active="activeIndex"
-    class="el-menu-demo index_row"
-    mode="horizontal"
-    @select="handleSelect"
-  >
-    <el-menu-item index="1"><a href="../contact_units/index.html">捷運局各單位窗口資訊</a></el-menu-item>
-    <el-menu-item index="2"><a href="../contact_com/index.html">各標號廠商窗口資訊</a></el-menu-item>
-  </el-menu>
+  <el-container>
+    <el-header>
+      <a href="../../index.html">
+        <h1>TARF2技術文件線上簽核系統</h1>
+      </a>
+      <h3>窗口資訊查詢</h3>
+      <p>蘇家淇(11548)</p>
+    </el-header>
+    <el-main>
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo index_row"
+        mode="horizontal"
+        @select="handleSelect"
+      >
+        <el-menu-item index="1"
+          ><a href="../contact_units/index.html"
+            >捷運局各單位窗口資訊</a
+          ></el-menu-item
+        >
+        <el-menu-item index="2"
+          ><a href="../contact_com/index.html"
+            >各標號廠商窗口資訊</a
+          ></el-menu-item
+        >
+      </el-menu>
 
-        <el-row>
-          <div class="title_main contact-title"><h4>捷運局各單位窗口資訊</h4></div>
-          <el-form class="contact-search">
-    <el-form-item>
-      <el-input
-      v-model="input1"
-      class="w-50 m-2"
-      size="large"
-      placeholder="搜尋"
-      :suffix-icon="Search"
-    />
-    </el-form-item>
-  </el-form>
-  
-  <el-table class="tableForm tableContactUnits" :data="data.newsdata" stripe border :header-cell-style="{ background: '#ebf4f9', color: '#000', textAlign: 'center'}">    
-    <el-table-column type="index" label="序號" width="90"/>
-    <el-table-column prop="depT_NAME" sortable label="單位" class="unitAlign"/>
-    <el-table-column prop="empL_NAME" sortable label="姓名"/>
-    <el-table-column prop="ofF_TEL" sortable label="電話"/>
-  </el-table>
-        </el-row>
-      </el-main>
-            <!-- <p>{{data.newsdata}}</p> -->
-    </el-container>
+      <el-row>
+        <div class="title_main contact-title">
+          <h4>捷運局各單位窗口資訊</h4>
+        </div>
+        <el-form class="contact-search">
+          <el-form-item>
+            <el-input
+              v-model="search"
+              class="w-50 m-2"
+              size="default"
+              placeholder="搜尋"
+              :suffix-icon="Search"
+            />
+          </el-form-item>
+        </el-form>
+
+        <el-table
+          class="tableForm tableContactUnits"
+          :data="tabledata.newsdata"
+          stripe
+          border
+          :header-cell-style="{
+            background: '#ebf4f9',
+            color: '#000',
+            textAlign: 'center',
+          }"
+        >
+          <el-table-column type="index" label="序號" width="90" />
+          <el-table-column
+            prop="depT_NAME"
+            sortable
+            label="單位"
+            class="unitAlign"
+          />
+          <el-table-column prop="empL_NAME" sortable label="姓名" />
+          <el-table-column prop="ofF_TEL" sortable label="電話" />
+        </el-table>
+      </el-row>
+    </el-main>
+    <p v-for="(item, i) in tabledata.newsdata">{{ item.ofF_TEL }}</p>
+  </el-container>
 </template>
-
-
