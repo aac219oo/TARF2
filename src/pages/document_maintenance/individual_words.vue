78f3214270a057690ddc5b802853e4f09db10139
JamesHsu <aac921oo@gmail.com>
2022-10-26 17:32:35 +0800

    some filter and addItem function problem

--- src/pages/document_maintenance/individual_words.vue
+++ src/pages/document_maintenance/individual_words.vue
@@ -1,310 +1,233 @@
 <script lang="ts" setup>
-<<<<<<< HEAD
-import { ref , computed } from 'vue'
+import { ref , computed , reactive} from 'vue'
 import { ElMessage, ElMessageBox } from 'element-plus'
-import { Search, Edit, Delete } from '@element-plus/icons-vue'
-=======
-import { ref } from "vue";
-import { ElMessage, ElMessageBox } from "element-plus";
-import { Search, Edit, Delete } from "@element-plus/icons-vue";
->>>>>>> dd6a50401f9b26033bc3f1eb19c8502116d7dc92
+import { Search, Edit, Delete, User } from '@element-plus/icons-vue'
 
 // navbar
 const activeIndex = ref("1");
 const handleSelect = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath);
+	console.log(key, keyPath);
 };
 
-<<<<<<< HEAD
 // const callback = '<%=Session["TEST_SESSION"].ToString()%>' 回傳API的值
 
-
 interface User {
-  wordsName: string
+	wordsName: string
 }
-=======
-const input3 = ref("");
->>>>>>> dd6a50401f9b26033bc3f1eb19c8502116d7dc92
 
 const search = ref('')
 const filterTableData = computed(() =>
-  tableData.filter(
-    (data) =>
-      !search.value ||
-      data.wordsName.toLowerCase().includes(search.value.toLowerCase())
-  )
+	tableData.filter(
+		(data) =>
+			!search.value ||
+			data.wordsName.toLowerCase().includes(search.value.toLowerCase())
+	)
 )
 
 const tableHeader = ref([
-  {
-    prop: "wordsName",
-    label: "詞彙名稱",
-    editable: false,
-    type: "input",
-  },
+	{
+		prop: "wordsName",
+		label: "詞彙名稱",
+		editable: false,
+		type: "input",
+	},
 ]);
 
-const item = {
-  wordsName: "",
-};
 
-<<<<<<< HEAD
-const tableData: User[] =[
-      {
-        wordsName: "test0",
-      },
-      {
-        wordsName: "test1",
-      },
-      {
-        wordsName: "test2",
-      },
-      {
-        wordsName: "test3",
-      },
-]
+const tableData = reactive<User[]>([
+			{
+				wordsName: "test0",
+			},
+			{
+				wordsName: "test1",
+			},
+			{
+				wordsName: "test2",
+			},
+			{
+				wordsName: "test3",
+			},
+])
 
-const item = {
-  wordsName: '',
-}
-
-const onAddItem = (item) => {
-  item.editable = true;
-  filterTableData.value.push({
-    wordsName: '',
-  })
-}
-=======
-const tableData = ref([
-  {
-    wordsName: "test",
-  },
-]);
->>>>>>> dd6a50401f9b26033bc3f1eb19c8502116d7dc92
-
-const handleEdit = (row) => {
-  row.editable = true;
-};
-
-const deleteRow = (index: number) => {
-<<<<<<< HEAD
-  filterTableData.value.splice(index, 1)
-}
-
-
-=======
-  tableData.value.splice(index, 1);
-};
->>>>>>> dd6a50401f9b26033bc3f1eb19c8502116d7dc92
 
 const onAddItem = (index) => {
-  const item = {
-    wordsName: "",
-    editable: true,
-  };
-  tableData.value.splice(index, 0, item);
+	const item = {
+		wordsName: "",
+		editable: true,
+	};
+	filterTableData.value.splice(index, 0, item)
+}
+
+const handleEdit = (row) => {
+	row.editable = true;
 };
+
+const deleteRow = (index: number, row) => {
+	row.value.splice(index, 1);
+};
+
 </script>
 <template>
-  <el-container>
-    <el-header>
-      <a href="../../index.html">
-        <h1>TARF2技術文件線上簽核系統</h1>
-      </a>
-      <h3>資料維護作業</h3>
-      <p>蘇家淇(11548)</p>
-    </el-header>
-    <el-main>
-      <el-menu
-        :default-active="activeIndex"
-        class="el-menu-demo index_row"
-        mode="horizontal"
-        @select="handleSelect"
-      >
-        <el-menu-item index="6"
-          ><a href="../access_setting/index.html">權限設定</a></el-menu-item
-        >
-        <el-menu-item index="1"
-          ><a href="../individual_words/index.html"
-            >個人使用詞彙</a
-          ></el-menu-item
-        >
-        <el-menu-item index="2"
-          ><a href="../item_codes/index.html">送審項目代碼</a></el-menu-item
-        >
-        <el-menu-item index="3"
-          ><a href="../resualt_codes/index.html">送審結果代碼</a></el-menu-item
-        >
-        <el-menu-item index="4"
-          ><a href="../condition_codes/index.html"
-            >案件狀態代碼</a
-          ></el-menu-item
-        >
-        <el-menu-item index="5"
-          ><a href="../com_using_right/index.html"
-            >廠商使用權限</a
-          ></el-menu-item
-        >
-      </el-menu>
+	<el-container>
+		<el-header>
+			<a href="../../index.html">
+				<h1>TARF2技術文件線上簽核系統</h1>
+			</a>
+			<h3>資料維護作業</h3>
+			<p>蘇家淇(11548)</p>
+		</el-header>
+		<el-main>
+			<el-menu
+				:default-active="activeIndex"
+				class="el-menu-demo index_row"
+				mode="horizontal"
+				@select="handleSelect"
+			>
+				<el-menu-item index="6"
+					><a href="../access_setting/index.html">權限設定</a></el-menu-item
+				>
+				<el-menu-item index="1"
+					><a href="../individual_words/index.html"
+						>個人使用詞彙</a
+					></el-menu-item
+				>
+				<el-menu-item index="2"
+					><a href="../item_codes/index.html">送審項目代碼</a></el-menu-item
+				>
+				<el-menu-item index="3"
+					><a href="../resualt_codes/index.html">送審結果代碼</a></el-menu-item
+				>
+				<el-menu-item index="4"
+					><a href="../condition_codes/index.html"
+						>案件狀態代碼</a
+					></el-menu-item
+				>
+				<el-menu-item index="5"
+					><a href="../com_using_right/index.html"
+						>廠商使用權限</a
+					></el-menu-item
+				>
+			</el-menu>
 
-      <div class="maintenance_h3">技術文件【個人使用詞彙】維護</div>
+			<div class="maintenance_h3">技術文件【個人使用詞彙】維護</div>
 
-      <div class="maintenance_tool_wrap">
-        <div class="maintenance_search">
-<<<<<<< HEAD
-    <el-input
-      v-model="search"
-      placeholder="搜尋"
-      class="input-with-select"      
-      :suffix-icon="Search"
-    >
-    </el-input>
-  </div>
-
-  
-</div>
-        <el-table 
-            :data="filterTableData" 
-            border 
-            stripe 
-            style="width: 100%;" 
-            :header-cell-style="{ background: '#ebf4f9', color: '#000', textAlign: 'center'}">
-            <el-table-column type="index" label="序號" width="70" min-width="50" :resizable="false"/>
-    <el-table-column 
-        :prop="item.prop"
-        :label="item.label"
-        v-for="(item, index) in tableHeader"
-        :key="item.prop"
-        :resizable="false"
-        >
-        <template #default="scope">
-          <div
-            v-show="item.editable || scope.row.editable"
-            class="editable-row"
-          >
-            <template v-if="item.type === 'input'">
-              <el-input
-                size="small"
-                v-model="scope.row[item.prop]"
-                :placeholder="`請輸入${item.label}`"
-                @change="handleEdit(scope.$index)"
-=======
-          <el-input
-            v-model="input3"
-            placeholder="搜尋"
-            class="input-with-select"
-            :suffix-icon="Search"
-          >
-          </el-input>
-        </div>
-      </div>
-      <el-table
-        :data="tableData"
-        border
-        stripe
-        style="width: 100%"
-        :header-cell-style="{
-          background: '#ebf4f9',
-          color: '#000',
-          textAlign: 'center',
-        }"
-      >
-        <el-table-column
-          type="index"
-          label="序號"
-          width="70"
-          min-width="50"
-          :resizable="false"
-        />
-        <el-table-column
-          :prop="item.prop"
-          :label="item.label"
-          v-for="(item, index) in tableHeader"
-          :key="item.prop"
-          :resizable="false"
-        >
-          <template #default="scope">
-            <div
-              v-show="item.editable || scope.row.editable"
-              class="editable-row"
-            >
-              <template v-if="item.type === 'input'">
-                <el-input
-                  size="small"
-                  v-model="scope.row[item.prop]"
-                  :placeholder="`請輸入${item.label}`"
-                  @change="handleEdit(scope.$index)"
-                />
-              </template>
-            </div>
-            <div
-              v-show="!item.editable && !scope.row.editable"
-              class="editable-row"
-            >
-              <span class="editable-row-span">{{ scope.row[item.prop] }}</span>
-            </div>
-          </template>
-        </el-table-column>
-        <el-table-column width="120px" align="right" :resizable="false">
-          <template #header>
-            <el-button class="maintenance_plus" @click="onAddItem"
-              ><img
-                src="../../assets/icon07.png"
-                style="width: 26px; vertical-align: bottom"
-                alt=""
-            /></el-button>
-          </template>
-          <template #default="scope">
-            <el-popconfirm
-              width="170px"
-              title="確定要刪除嗎?"
-              confirm-button-text="是"
-              cancel-button-text="否"
-              confirm-button-type="danger"
-              cancel-button-type="primary"
-              @confirm="deleteRow(scope.$index)"
-            >
-              <template #reference>
-                <el-button>
-                  <img
-                    src="../../assets/icon06.png"
-                    style="width: 24px; vertical-align: bottom"
-                    alt=""
-                  />
-                </el-button>
-              </template>
-            </el-popconfirm>
-            <el-button
-              link
-              size="small"
-              v-show="!scope.row.editable"
-              @click="scope.row.editable = true"
-              class="button_edit"
-            >
-              <img
-                src="../../assets/icon04.png"
-                style="width: 24px; vertical-align: bottom"
-                alt=""
->>>>>>> dd6a50401f9b26033bc3f1eb19c8502116d7dc92
-              />
-            </el-button>
-            <el-button
-              link
-              size="small"
-              v-show="scope.row.editable"
-              @click="scope.row.editable = false"
-              class="button_edit"
-            >
-              <img
-                src="../../assets/icon05.png"
-                style="width: 24px; vertical-align: bottom"
-                alt=""
-              />
-            </el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-      <!-- <table_words /> -->
-    </el-main>
-  </el-container>
+			<div class="maintenance_tool_wrap">
+				<div class="maintenance_search">
+					<el-input
+						v-model="search"
+						placeholder="搜尋"
+						class="input-with-select"
+						:suffix-icon="Search"
+					>
+					</el-input>
+				</div>
+			</div>
+			<el-table
+				:data="filterTableData"
+				border
+				stripe
+				style="width: 100%"
+				:header-cell-style="{
+					background: '#ebf4f9',
+					color: '#000',
+					textAlign: 'center',
+				}"
+			>
+				<el-table-column
+					type="index"
+					label="序號"
+					width="70"
+					min-width="50"
+					:resizable="false"
+				/>
+				<el-table-column
+					:prop="item.prop"
+					:label="item.label"
+					v-for="(item, index) in tableHeader"
+					:key="item.prop"
+					:resizable="false"
+				>
+					<template #default="scope">
+						<div
+							v-show="item.editable || scope.row.editable"
+							class="editable-row"
+						>
+							<template v-if="item.type === 'input'">
+								<el-input
+									size="small"
+									v-model="scope.row[item.prop]"
+									:placeholder="`請輸入${item.label}`"
+									@change="handleEdit(scope.$index)"
+								/>
+							</template>
+						</div>
+						<div
+							v-show="!item.editable && !scope.row.editable"
+							class="editable-row"
+						>
+							<span class="editable-row-span">{{ scope.row[item.prop] }}</span>
+						</div>
+					</template>
+				</el-table-column>
+				<el-table-column width="120px" align="right" :resizable="false">
+					<template #header>
+						<el-button class="maintenance_plus" @click="onAddItem"
+							><img
+								src="../../assets/icon07.png"
+								style="width: 26px; vertical-align: bottom"
+								alt=""
+						/></el-button>
+					</template>
+					<template #default="scope">
+						<el-popconfirm
+							width="170px"
+							title="確定要刪除嗎?"
+							confirm-button-text="是"
+							cancel-button-text="否"
+							confirm-button-type="danger"
+							cancel-button-type="primary"
+							@confirm="deleteRow(scope.$index)"
+						>
+							<template #reference>
+								<el-button>
+									<img
+										src="../../assets/icon06.png"
+										style="width: 24px; vertical-align: bottom"
+										alt=""
+									/>
+								</el-button>
+							</template>
+						</el-popconfirm>
+						<el-button
+							link
+							size="small"
+							v-show="!scope.row.editable"
+							@click="scope.row.editable = true"
+							class="button_edit"
+						>
+							<img
+								src="../../assets/icon04.png"
+								style="width: 24px; vertical-align: bottom"
+								alt=""
+							/>
+						</el-button>
+						<el-button
+							link
+							size="small"
+							v-show="scope.row.editable"
+							@click="scope.row.editable = false"
+							class="button_edit"
+						>
+							<img
+								src="../../assets/icon05.png"
+								style="width: 24px; vertical-align: bottom"
+								alt=""
+							/>
+						</el-button>
+					</template>
+				</el-table-column>
+			</el-table>
+			<!-- <table_words /> -->
+		</el-main>
+	</el-container>
 </template>
--- src/pages/document_search/contact_units.vue
+++ src/pages/document_search/contact_units.vue
@@ -1,13 +1,7 @@
 <script lang="ts" setup>
-<<<<<<< HEAD
 import { ref , reactive , onMounted , watch , computed } from 'vue'
 import { Search } from '@element-plus/icons-vue'
 import axios from 'axios';
-=======
-import { ref, reactive, onMounted, watch, computed } from "vue";
-import { Search } from "@element-plus/icons-vue";
-import axios from "axios";
->>>>>>> dd6a50401f9b26033bc3f1eb19c8502116d7dc92
 
 //navbar
 const activeIndex = ref("1");
@@ -15,54 +9,40 @@ const handleSelect = (key: string, keyPath: string[]) => {
   console.log(key, keyPath);
 };
 
-interface User {
-  date: string
-  name: string
-  address: string
-}
+// interface User {
+//   date: string
+//   name: string
+//   address: string
+// }
 
 // filter
-<<<<<<< HEAD
 const search = ref('')
 
-const tabledata = reactive({
-  newsdata: '',
-  filterTableData: computed(() =>
-  xxData.filter(
-    (data) =>
-      !search.value ||
-      data.name.toLowerCase().includes(search.value.toLowerCase())
-  )
-)
-})
+// const tabledata = reactive({
+//   newsdata: '',
+//   filterTableData: computed(() =>
+//   xxData.filter(
+//     (data) =>
+//       !search.value ||
+//       data.name.toLowerCase().includes(search.value.toLowerCase())
+//   )
+// )
+// })
 
 // const callback = '<%=Session["TEST_SESSION"].ToString()%>' 回傳API的值
-=======
-const search = ref("");
 
 const tabledata = reactive({
-  newsdata: null,
-});
->>>>>>> dd6a50401f9b26033bc3f1eb19c8502116d7dc92
+    newsdata:'',
+})
 
 // axios
 const url = "http://tarf.grp.com.tw/api/Test/GetDeptChargQuery";
 
 onMounted(() => {
-<<<<<<< HEAD
   axios.get(url)
     .then(res => {
-      console.log(res);
+      console.log(res.data);
       tabledata.newsdata = res.data;
-=======
-  axios
-    .get(url)
-    .then((res) => {
-      // console.log(res.data);
-      // tabledata.newsdata = res.data;
-      console.log(res);
-      tabledata.newsdata = res.data.newsdata;
->>>>>>> dd6a50401f9b26033bc3f1eb19c8502116d7dc92
     })
     .catch(function (error) {
       // handle error
@@ -73,59 +53,7 @@ onMounted(() => {
     });
 });
 
-// console.log(tabledata);
 
-<<<<<<< HEAD
-=======
-// table
-// const tableInfo = [
-//         {
-//     depT_NAME: '綜規處',
-//     empL_NAME: '李心平',
-//     ofF_TEL: '02-25215550-8130',
-//         },{
-//     depT_NAME: '土建處',
-//     empL_NAME: '徐興華',
-//     ofF_TEL: '02-25215550-8255',
-//         },{
-//     depT_NAME: '機設處',
-//     empL_NAME: '廖乃寬',
-//     ofF_TEL: '02-25215550-8319',
-//         },{
-//     depT_NAME: '工管處',
-//     empL_NAME: '李昭瑢',
-//     ofF_TEL: '02-25215550-8449',
-//         },{
-//     depT_NAME: '技術處',
-//     empL_NAME: '林麗卿',
-//     ofF_TEL: '02-25215550-8022',
-//         },{
-//     depT_NAME: '秘書室',
-//     empL_NAME: '林俐妘',
-//     ofF_TEL: '02-25215550-8269',
-//         },{
-//     depT_NAME: '會計室',
-//     empL_NAME: '葉桂英',
-//     ofF_TEL: '02-25215550-262',
-//         },{
-//     depT_NAME: '開發處',
-//     empL_NAME: '詹麗燕',
-//     ofF_TEL: '02-25215550-23',
-//         },{
-//     depT_NAME: '一區工程處土一所',
-//     empL_NAME: '黃秋香',
-//     ofF_TEL: '02-25215550-18',
-//         },{
-//     depT_NAME: '一區工程處土二所',
-//     empL_NAME: '張瑛敏',
-//     ofF_TEL: '02-25215550-19',
-//         },{
-//     depT_NAME: '一區工程處土三所',
-//     empL_NAME: '張美惠',
-//     ofF_TEL: '02-25215550-13',
-//         },
-// ]
->>>>>>> dd6a50401f9b26033bc3f1eb19c8502116d7dc92
 </script>
 <template>
   <el-container>
@@ -155,7 +83,6 @@ onMounted(() => {
         >
       </el-menu>
 
-<<<<<<< HEAD
         <el-row>
           <div class="title_main contact-title"><h4>捷運局各單位窗口資訊</h4></div>
           <el-form class="contact-search">
@@ -180,47 +107,4 @@ onMounted(() => {
       </el-main>
             <p>{{tabledata.newsdata}}</p>
     </el-container>
-=======
-      <el-row>
-        <div class="title_main contact-title">
-          <h4>捷運局各單位窗口資訊</h4>
-        </div>
-        <el-form class="contact-search">
-          <el-form-item>
-            <el-input
-              v-model="search"
-              class="w-50 m-2"
-              size="default"
-              placeholder="搜尋"
-              :suffix-icon="Search"
-            />
-          </el-form-item>
-        </el-form>
-
-        <el-table
-          class="tableForm tableContactUnits"
-          :data="tabledata.newsdata"
-          stripe
-          border
-          :header-cell-style="{
-            background: '#ebf4f9',
-            color: '#000',
-            textAlign: 'center',
-          }"
-        >
-          <el-table-column type="index" label="序號" width="90" />
-          <el-table-column
-            prop="depT_NAME"
-            sortable
-            label="單位"
-            class="unitAlign"
-          />
-          <el-table-column prop="empL_NAME" sortable label="姓名" />
-          <el-table-column prop="ofF_TEL" sortable label="電話" />
-        </el-table>
-      </el-row>
-    </el-main>
-    <p v-for="(item, i) in tabledata.newsdata">{{ item.ofF_TEL }}</p>
-  </el-container>
->>>>>>> dd6a50401f9b26033bc3f1eb19c8502116d7dc92
 </template>
